425e55aee01b499d871d0d74848a5eb76085f7c8
GGO-web <iicandyoii@gmail.com>
2022-11-30 15:52:15 +0200

    fixup! Added US8-WasteIdentifier testing build

--- src/App.tsx
+++ src/App.tsx
@@ -2,7 +2,7 @@ import React from 'react';
 
 import { ToastContainer } from 'react-toastify';
 
-import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import { HashRouter as Router, Route, Routes } from 'react-router-dom';
 
 import { Home } from './pages/Home/Home';
 import { TypesOfWaste } from './pages/TypesOfWaste/TypesOfWaste';
@@ -10,14 +10,14 @@ import { TrashInfo } from './pages/TrashInfo/TrashInfo';
 
 function App() {
   return (
-    <BrowserRouter>
+    <Router>
       <ToastContainer limit={1} />
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/detailed" element={<TypesOfWaste />} />
         <Route path="/detailed/:id" element={<TrashInfo />} />
       </Routes>
-    </BrowserRouter>
+    </Router>
   );
 }
 
