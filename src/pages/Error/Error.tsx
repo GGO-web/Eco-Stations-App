156269e9cf4c9a8750bacec81b19638a48c21cc7
unknown <artemix926@gmail.com>
2022-12-01 01:17:55 +0200

    fixed design

---
+++ src/pages/Error/Error.tsx
@@ -0,0 +1,16 @@
+import React from 'react';
+
+import { Link } from 'react-router-dom';
+
+export function Error() {
+  return (
+    <div className="grid place-items-center w-full h-screen p-5 font-bold">
+      <div className="flex flex-col items-center text-dark">
+        <h3 className="text-[220px] max-[425px]:text-[160px] leading-none">404</h3>
+        <p className="text-6xl tracking-widest">THE PAGE</p>
+        <p className="text-2xl mt-2">WAS NOT FOUND</p>
+        <Link to="/" className="mt-3 bg-light-green  rounded-2xl px-8 py-4">BACK TO HOME</Link>
+      </div>
+    </div>
+  );
+}
--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -5,6 +5,13 @@ module.exports = {
   ],
   theme: {
     extend: {
+      colors: {
+        'main': '#5CDB95',
+        'light-green': '#8EE4AF',
+        'light': 'rgba(237, 245, 225, 0.5)',
+        'dark-green': '#379683',
+        'dark': '#05386B',
+      },
       gridTemplateColumns: {
         'auto-types': 'repeat(auto-fit, minmax(420px, 1fr))'
       }
