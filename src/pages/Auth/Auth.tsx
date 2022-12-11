a8e7436537a71f959c48780d0109fda58c3b9781
GGO-web <iicandyoii@gmail.com>
2022-12-11 15:08:52 +0200

    Changed styles of auth & login pages

--- src/pages/Auth/Auth.tsx
+++ src/pages/Auth/Auth.tsx
@@ -1,30 +1,17 @@
-import React, { useState } from 'react';
+import React, { useMemo, useState } from 'react';
 
 import { useNavigate } from 'react-router-dom';
-
-import {
-  Box,
-  Button,
-  Card,
-  CardActions,
-  CardContent,
-  FormControl,
-  IconButton,
-  InputAdornment,
-  InputLabel,
-  MenuItem,
-  OutlinedInput,
-  Select,
-  SelectChangeEvent,
-  TextField,
-  Typography,
-} from '@mui/material';
 
 import jwt_decode from 'jwt-decode';
 
 import { toast } from 'react-toastify';
 
 import { v4 } from 'uuid';
+
+import Select, {
+  SingleValue,
+} from 'react-select';
+
 import { useUserRegisterMutation } from '../../redux/services/auth';
 
 import { useActions } from '../../hooks/actions';
@@ -50,6 +37,29 @@ export function Auth() {
 
   const navigate = useNavigate();
 
+  const roleOptions = useMemo(() => ([
+    { value: 'User', label: 'User' },
+    { value: 'Service Provider', label: 'Service Provider' },
+  ]), []);
+
+  const roleColourStyles: any = {
+    menuOption: (styles: any) => ({
+      ...styles,
+      background: '#8EE4AF',
+    }),
+    singleValue: (provided: any, { hasValue }: any) => ({
+      ...provided,
+      color: !hasValue ? 'gray' : '#05386B',
+    }),
+    control: (provided: any) => ({
+      ...provided,
+      border: '2px solid #05386B',
+      padding: '5px 7px',
+      fontSize: '0.875rem',
+      marginTop: '0',
+    }),
+  };
+
   const handleChange = (prop: keyof IAuth) => (event: React.ChangeEvent<HTMLInputElement>) => {
     setValues({ ...values, [prop]: event.target.value });
   };
@@ -65,8 +75,8 @@ export function Auth() {
     event.preventDefault();
   };
 
-  const handleChangeRole = (event: SelectChangeEvent) => {
-    setUserRole(event.target.value as string);
+  const handleChangeRole = (newRole: SingleValue<{ value: string, label: string }>) => {
+    setUserRole(newRole?.value as string);
   };
 
   const handleAuth = async () => {
@@ -109,6 +119,16 @@ export function Auth() {
       });
       navigate('/');
     } catch (err: any) {
+      if (!err.data) {
+        toast.error(`Something goes wrong 😨 - ${err.error.split(' ')[1]}`, {
+          toastId: 'error-msg',
+          position: toast.POSITION.TOP_RIGHT,
+          autoClose: 2500,
+        });
+
+        return;
+      }
+
       const toastErrors = (err as TError).data.message.map(
         (errorMessage) => (
           <div key={v4()}>
@@ -127,89 +147,80 @@ export function Auth() {
 
   return (
     <div className="grid place-items-center fixed w-full h-screen bg-light">
-      <Card sx={{
-        width: 480,
-        paddingBottom: '20px',
-      }}
+      <form
+        className="auth-form rounded-[20px] bg-white p-5 w-[480px]"
+        action="#"
+        onSubmit={(e) => e.preventDefault()}
       >
-        <CardContent sx={{ display: 'flex', flexDirection: 'column', gap: '10px' }}>
-          <Typography sx={{ fontSize: 30 }} color="text.secondary">
+        <div className="card__content flex flex-col gap-[10px] mb-6">
+          <p className="text-blue-400 text-2xl mb-5">
             Create a new account
-          </Typography>
+          </p>
 
-          <Box sx={{ display: 'flex' }}>
-            <TextField
-              sx={{ width: '50ch', outlineColor: 'none' }}
-              helperText="Please enter your name"
-              id="demo-helper-text-aligned"
-              label="Name"
+          <label className="auth-form__group">
+            <span className="block mb-2">Name</span>
+
+            <input
+              className="input"
+              placeholder="Please enter your name"
               value={values.username}
               onChange={handleChange('username')}
             />
-          </Box>
+          </label>
 
-          <Box sx={{ display: 'flex' }}>
-            <TextField
-              sx={{ width: '50ch' }}
-              type="email"
-              helperText="Please enter your e-mail"
-              id="demo-helper-text-aligned"
-              label="Email"
+          <label className="auth-form__group">
+            <span className="block mb-2">Email</span>
+
+            <input
+              className="input"
+              placeholder="Please enter your email"
               value={values.email}
               onChange={handleChange('email')}
             />
-          </Box>
+          </label>
 
-          <Box sx={{ minWidth: 200, textAlign: 'start' }}>
-            <FormControl sx={{ minWidth: 100 }}>
-              <InputLabel id="demo-simple-select-label">Role</InputLabel>
-              <Select
-                labelId="demo-simple-select-label"
-                id="demo-simple-select"
-                value={userRole}
-                label="Role"
-                onChange={handleChangeRole}
-              >
-                <MenuItem value="User">User</MenuItem>
-                <MenuItem value="Service">Service Provider</MenuItem>
-              </Select>
-            </FormControl>
-          </Box>
+          <label className="auth-form__group">
+            <span className="block mb-2">Password</span>
 
-          <Box sx={{ display: 'flex' }}>
-            <FormControl sx={{ width: '50ch' }} variant="outlined">
-              <InputLabel htmlFor="outlined-adornment-password">Password</InputLabel>
-
-              <OutlinedInput
-                id="outlined-adornment-password"
-                type={values.showPassword ? 'text' : 'password'}
+            <div className="relative">
+              <input
+                className="input"
+                placeholder="Please enter your password"
                 value={values.password}
+                type={!values.showPassword ? 'password' : 'text'}
                 onChange={handleChange('password')}
-                endAdornment={(
-                  <InputAdornment position="end">
-                    <IconButton
-                      aria-label="toggle password visibility"
-                      onClick={handleClickShowPassword}
-                      onMouseDown={handleMouseDownPassword}
-                      edge="end"
-                    >
-                      {values.showPassword
-                        ? <svg width="24" height="24" className="MuiSvgIcon-root MuiSvgIcon-fontSizeMedium css-i4bv87-MuiSvgIcon-root" focusable="false" aria-hidden="true" viewBox="0 0 24 24" data-testid="VisibilityOffIcon"><path d="M12 7c2.76 0 5 2.24 5 5 0 .65-.13 1.26-.36 1.83l2.92 2.92c1.51-1.26 2.7-2.89 3.43-4.75-1.73-4.39-6-7.5-11-7.5-1.4 0-2.74.25-3.98.7l2.16 2.16C10.74 7.13 11.35 7 12 7zM2 4.27l2.28 2.28.46.46C3.08 8.3 1.78 10.02 1 12c1.73 4.39 6 7.5 11 7.5 1.55 0 3.03-.3 4.38-.84l.42.42L19.73 22 21 20.73 3.27 3 2 4.27zM7.53 9.8l1.55 1.55c-.05.21-.08.43-.08.65 0 1.66 1.34 3 3 3 .22 0 .44-.03.65-.08l1.55 1.55c-.67.33-1.41.53-2.2.53-2.76 0-5-2.24-5-5 0-.79.2-1.53.53-2.2zm4.31-.78 3.15 3.15.02-.16c0-1.66-1.34-3-3-3l-.17.01z" /></svg>
-                        : <svg width="24" height="24" className="MuiSvgIcon-root MuiSvgIcon-fontSizeMedium css-i4bv87-MuiSvgIcon-root" focusable="false" aria-hidden="true" viewBox="0 0 24 24" data-testid="VisibilityIcon"><path d="M12 4.5C7 4.5 2.73 7.61 1 12c1.73 4.39 6 7.5 11 7.5s9.27-3.11 11-7.5c-1.73-4.39-6-7.5-11-7.5zM12 17c-2.76 0-5-2.24-5-5s2.24-5 5-5 5 2.24 5 5-2.24 5-5 5zm0-8c-1.66 0-3 1.34-3 3s1.34 3 3 3 3-1.34 3-3-1.34-3-3-3z" /></svg>}
-                    </IconButton>
-                  </InputAdornment>
-                )}
-                label="Password"
               />
-            </FormControl>
-          </Box>
-        </CardContent>
 
-        <CardActions>
-          <Button sx={{ marginLeft: 1 }} variant="outlined" onClick={handleAuth}>Sign up</Button>
-        </CardActions>
-      </Card>
+              <button
+                className="absolute top-[50%] bg-white p-1 right-3 translate-y-[-50%]"
+                type="button"
+                aria-label="toggle password visibility"
+                onClick={handleClickShowPassword}
+                onMouseDown={handleMouseDownPassword}
+              >
+                {values.showPassword
+                  ? <svg width="24" height="24" className="MuiSvgIcon-root MuiSvgIcon-fontSizeMedium css-i4bv87-MuiSvgIcon-root" focusable="false" aria-hidden="true" viewBox="0 0 24 24" data-testid="VisibilityOffIcon"><path d="M12 7c2.76 0 5 2.24 5 5 0 .65-.13 1.26-.36 1.83l2.92 2.92c1.51-1.26 2.7-2.89 3.43-4.75-1.73-4.39-6-7.5-11-7.5-1.4 0-2.74.25-3.98.7l2.16 2.16C10.74 7.13 11.35 7 12 7zM2 4.27l2.28 2.28.46.46C3.08 8.3 1.78 10.02 1 12c1.73 4.39 6 7.5 11 7.5 1.55 0 3.03-.3 4.38-.84l.42.42L19.73 22 21 20.73 3.27 3 2 4.27zM7.53 9.8l1.55 1.55c-.05.21-.08.43-.08.65 0 1.66 1.34 3 3 3 .22 0 .44-.03.65-.08l1.55 1.55c-.67.33-1.41.53-2.2.53-2.76 0-5-2.24-5-5 0-.79.2-1.53.53-2.2zm4.31-.78 3.15 3.15.02-.16c0-1.66-1.34-3-3-3l-.17.01z" /></svg>
+                  : <svg width="24" height="24" className="MuiSvgIcon-root MuiSvgIcon-fontSizeMedium css-i4bv87-MuiSvgIcon-root" focusable="false" aria-hidden="true" viewBox="0 0 24 24" data-testid="VisibilityIcon"><path d="M12 4.5C7 4.5 2.73 7.61 1 12c1.73 4.39 6 7.5 11 7.5s9.27-3.11 11-7.5c-1.73-4.39-6-7.5-11-7.5zM12 17c-2.76 0-5-2.24-5-5s2.24-5 5-5 5 2.24 5 5-2.24 5-5 5zm0-8c-1.66 0-3 1.34-3 3s1.34 3 3 3 3-1.34 3-3-1.34-3-3-3z" /></svg>}
+              </button>
+            </div>
+          </label>
+
+          <div className="auth-form__group">
+            <span className="block mb-2">Role</span>
+
+            <Select
+              onChange={(newRole: SingleValue<typeof roleOptions[0]>) => handleChangeRole(newRole)}
+              placeholder="Select role"
+              styles={roleColourStyles}
+              className="react-select-container"
+              classNamePrefix="react-select"
+              options={roleOptions as any}
+            />
+          </div>
+        </div>
+
+        <button className="p-3 rounded-2xl bg-[#7483bd] text-white w-full" type="button" onClick={handleAuth}>Sign up</button>
+      </form>
     </div>
-
   );
 }
--- src/pages/Login/LoginPage.tsx
+++ src/pages/Login/LoginPage.tsx
@@ -1,30 +1,18 @@
 import React, { ChangeEvent, useState } from 'react';
 
-import {
-  Box,
-  Button,
-  Card,
-  CardActions,
-  CardContent,
-  FormControl,
-  IconButton,
-  InputAdornment,
-  InputLabel,
-  OutlinedInput,
-  TextField,
-  Typography,
-} from '@mui/material';
-
 import { toast } from 'react-toastify';
+
 import jwt_decode from 'jwt-decode';
-import { useNavigate } from 'react-router-dom';
+
 import { v4 } from 'uuid';
-import { ILoginState } from '../../models/login.model';
 
-import { useUserLoginMutation } from '../../redux/services/auth';
-
+import { useNavigate } from 'react-router-dom';
 import { useActions } from '../../hooks/actions';
 import { useLocalStorage } from '../../hooks/localStorage';
+import { useUserLoginMutation } from '../../redux/services/auth';
+
+import { ILoginState } from '../../models/login.model';
+
 import { AUTH_CREDENTIALS, LOGIN_STATUS_DESCRIPTION, TError } from '../../constants';
 
 export function LoginPage() {
@@ -91,6 +79,16 @@ export function LoginPage() {
 
       navigate('/');
     } catch (err: any) {
+      if (!err.data) {
+        toast.error(`Something goes wrong 😨 - ${err.error.split(' ')[1]}`, {
+          toastId: 'error-msg',
+          position: toast.POSITION.TOP_RIGHT,
+          autoClose: 2500,
+        });
+
+        return;
+      }
+
       const toastErrors = (err as TError).data.message.map(
         (errorMessage) => (
           <div key={v4()}>
@@ -109,63 +107,56 @@ export function LoginPage() {
 
   return (
     <div className="grid place-items-center fixed w-full h-screen bg-light">
-      <Card sx={{
-        width: 400,
-        paddingBottom: '20px',
-      }}
+      <form
+        className="auth-form rounded-[20px] bg-white p-5 w-[480px]"
+        action="#"
+        onSubmit={(e) => e.preventDefault()}
       >
-        <CardContent>
-          <Box sx={{ display: 'flex' }}>
-            <Typography sx={{ fontSize: 30 }} color="text.secondary" gutterBottom>
-              Login
-            </Typography>
-          </Box>
+        <div className="card__content flex flex-col gap-[10px] mb-6">
+          <p className="text-blue-400 text-2xl mb-5">
+            Login
+          </p>
 
-          <Box sx={{ display: 'flex', marginBottom: '20px' }}>
-            <TextField
-              sx={{ width: '50ch' }}
-              helperText="Please enter your e-mail"
-              id="demo-helper-text-aligned"
-              label="Email"
+          <label className="auth-form__group">
+            <span className="block mb-2">Email</span>
+
+            <input
+              className="input"
+              placeholder="Please enter your email"
               value={values.username}
               onChange={handleChange('username')}
             />
-          </Box>
-          <Box sx={{ minWidth: 100, textAlign: 'start' }} />
+          </label>
 
-          <Box sx={{ display: 'flex' }}>
-            <FormControl sx={{ width: '50ch' }} variant="outlined">
-              <InputLabel htmlFor="outlined-adornment-password">Password</InputLabel>
+          <label className="auth-form__group">
+            <span className="block mb-2">Password</span>
 
-              <OutlinedInput
-                id="outlined-adornment-password"
-                type={values.showPassword ? 'text' : 'password'}
+            <div className="relative">
+              <input
+                className="input"
+                placeholder="Please enter your password"
                 value={values.password}
+                type={!values.showPassword ? 'password' : 'text'}
                 onChange={handleChange('password')}
-                endAdornment={(
-                  <InputAdornment position="end">
-                    <IconButton
-                      aria-label="toggle password visibility"
-                      onClick={handleClickShowPassword}
-                      onMouseDown={handleMouseDownPassword}
-                      edge="end"
-                    >
-                      {values.showPassword
-                        ? <svg width="24" height="24" className="MuiSvgIcon-root MuiSvgIcon-fontSizeMedium css-i4bv87-MuiSvgIcon-root" focusable="false" aria-hidden="true" viewBox="0 0 24 24" data-testid="VisibilityOffIcon"><path d="M12 7c2.76 0 5 2.24 5 5 0 .65-.13 1.26-.36 1.83l2.92 2.92c1.51-1.26 2.7-2.89 3.43-4.75-1.73-4.39-6-7.5-11-7.5-1.4 0-2.74.25-3.98.7l2.16 2.16C10.74 7.13 11.35 7 12 7zM2 4.27l2.28 2.28.46.46C3.08 8.3 1.78 10.02 1 12c1.73 4.39 6 7.5 11 7.5 1.55 0 3.03-.3 4.38-.84l.42.42L19.73 22 21 20.73 3.27 3 2 4.27zM7.53 9.8l1.55 1.55c-.05.21-.08.43-.08.65 0 1.66 1.34 3 3 3 .22 0 .44-.03.65-.08l1.55 1.55c-.67.33-1.41.53-2.2.53-2.76 0-5-2.24-5-5 0-.79.2-1.53.53-2.2zm4.31-.78 3.15 3.15.02-.16c0-1.66-1.34-3-3-3l-.17.01z" /></svg>
-                        : <svg width="24" height="24" className="MuiSvgIcon-root MuiSvgIcon-fontSizeMedium css-i4bv87-MuiSvgIcon-root" focusable="false" aria-hidden="true" viewBox="0 0 24 24" data-testid="VisibilityIcon"><path d="M12 4.5C7 4.5 2.73 7.61 1 12c1.73 4.39 6 7.5 11 7.5s9.27-3.11 11-7.5c-1.73-4.39-6-7.5-11-7.5zM12 17c-2.76 0-5-2.24-5-5s2.24-5 5-5 5 2.24 5 5-2.24 5-5 5zm0-8c-1.66 0-3 1.34-3 3s1.34 3 3 3 3-1.34 3-3-1.34-3-3-3z" /></svg>}
-                    </IconButton>
-                  </InputAdornment>
-                )}
-                label="Password"
               />
-            </FormControl>
-          </Box>
-        </CardContent>
 
-        <CardActions>
-          <Button sx={{ marginLeft: 1 }} variant="outlined" onClick={handleLogin}>Sign in</Button>
-        </CardActions>
-      </Card>
+              <button
+                className="absolute top-[50%] bg-white p-1 right-3 translate-y-[-50%]"
+                type="button"
+                aria-label="toggle password visibility"
+                onClick={handleClickShowPassword}
+                onMouseDown={handleMouseDownPassword}
+              >
+                {values.showPassword
+                  ? <svg width="24" height="24" className="MuiSvgIcon-root MuiSvgIcon-fontSizeMedium css-i4bv87-MuiSvgIcon-root" focusable="false" aria-hidden="true" viewBox="0 0 24 24" data-testid="VisibilityOffIcon"><path d="M12 7c2.76 0 5 2.24 5 5 0 .65-.13 1.26-.36 1.83l2.92 2.92c1.51-1.26 2.7-2.89 3.43-4.75-1.73-4.39-6-7.5-11-7.5-1.4 0-2.74.25-3.98.7l2.16 2.16C10.74 7.13 11.35 7 12 7zM2 4.27l2.28 2.28.46.46C3.08 8.3 1.78 10.02 1 12c1.73 4.39 6 7.5 11 7.5 1.55 0 3.03-.3 4.38-.84l.42.42L19.73 22 21 20.73 3.27 3 2 4.27zM7.53 9.8l1.55 1.55c-.05.21-.08.43-.08.65 0 1.66 1.34 3 3 3 .22 0 .44-.03.65-.08l1.55 1.55c-.67.33-1.41.53-2.2.53-2.76 0-5-2.24-5-5 0-.79.2-1.53.53-2.2zm4.31-.78 3.15 3.15.02-.16c0-1.66-1.34-3-3-3l-.17.01z" /></svg>
+                  : <svg width="24" height="24" className="MuiSvgIcon-root MuiSvgIcon-fontSizeMedium css-i4bv87-MuiSvgIcon-root" focusable="false" aria-hidden="true" viewBox="0 0 24 24" data-testid="VisibilityIcon"><path d="M12 4.5C7 4.5 2.73 7.61 1 12c1.73 4.39 6 7.5 11 7.5s9.27-3.11 11-7.5c-1.73-4.39-6-7.5-11-7.5zM12 17c-2.76 0-5-2.24-5-5s2.24-5 5-5 5 2.24 5 5-2.24 5-5 5zm0-8c-1.66 0-3 1.34-3 3s1.34 3 3 3 3-1.34 3-3-1.34-3-3-3z" /></svg>}
+              </button>
+            </div>
+          </label>
+        </div>
+
+        <button className="p-3 rounded-2xl bg-[#7483bd] text-white w-full" type="button" onClick={handleLogin}>Sign in</button>
+      </form>
     </div>
 
   );
