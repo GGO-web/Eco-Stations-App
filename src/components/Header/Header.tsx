a66fdf8472420cc3bd1ad8e8428bff9ffc05dca4
GGO-web <iicandyoii@gmail.com>
2022-12-11 10:44:56 +0200

    Removed header from login & auth pages

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -19,7 +19,6 @@ export function Header() {
   return (
     <header
       style={{
-        // position: 'sticky',
         background: '#8EE4AF',
         color: '#05386B',
         display: 'flex',
@@ -29,11 +28,11 @@ export function Header() {
         overflow: 'hidden',
       }}
     >
-      <NavLink className="h-[100%]" to="/">
+      <NavLink className="h-[70px]" to="/">
         <img src="ecofinder.svg" alt="Logo" className="bg-white h-full object-cover object-center w-[300px]" />
       </NavLink>
 
-      <div className="header-menu flex items-center gap-2 pl-5 pr-5">
+      <div className="header-menu flex items-center gap-2 pl-3 pr-3">
         <div className="flex gap-4 items-center">
           <NavLink className="text-xl font-bold py-2 px-2 hover:bg-dark hover:text-light-green transition rounded-xl" to="/detailed">Waste Types</NavLink>
 
@@ -44,10 +43,10 @@ export function Header() {
           {!isAuth ? (
             <button
               type="button"
-              className="py-1 px-1 rounded-full hover:bg-light transition"
+              className="rounded-full hover:bg-light transition"
               onClick={() => setUserMenuOpen(!userMenuOpen)}
             >
-              <svg focusable="false" aria-hidden="true" width="32" height="32" viewBox="0 0 24 24">
+              <svg className="p-[2px]" focusable="false" aria-hidden="true" width="30" height="30" viewBox="0 0 24 24">
                 <path fill="currentColor" d="M12 2C6.48 2 2 6.48 2 12s4.48 10 10 10 10-4.48 10-10S17.52 2 12 2zm0 4c1.93 0 3.5 1.57 3.5 3.5S13.93 13 12 13s-3.5-1.57-3.5-3.5S10.07 6 12 6zm0 14c-2.03 0-4.43-.82-6.14-2.88C7.55 15.8 9.68 15 12 15s4.45.8 6.14 2.12C16.43 19.18 14.03 20 12 20z" />
               </svg>
             </button>
