7ff45932c66bf88571b85bfc11834588b02e0c6e
GGO-web <iicandyoii@gmail.com>
2022-12-13 14:16:47 +0200

    Fixed authorization problem with service provider option

--- src/pages/Auth/Auth.tsx
+++ src/pages/Auth/Auth.tsx
@@ -42,7 +42,7 @@ export function Auth() {
 
   const roleOptions = useMemo(() => ([
     { value: 'User', label: 'User' },
-    { value: 'Service Provider', label: 'Service Provider' },
+    { value: 'Service', label: 'Service Provider' },
   ]), []);
 
   const roleColourStyles: any = {
